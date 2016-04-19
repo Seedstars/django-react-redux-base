698612339d191b21d250ad7f35189223c73da9c5
Luis Rodrigues <lfrodrigues@gmail.com>
2016-04-19 20:51:27 +0100

    Fixed formating for files

--- webpack/common.config.js
+++ webpack/common.config.js
@@ -14,110 +14,112 @@ require('babel-polyfill').default;
 const TARGET = process.env.npm_lifecycle_event;
 
 const PATHS = {
-  app: path.join(__dirname, '../src/static'),
-  build: path.join(__dirname, '../src/static_dist'),
+    app: path.join(__dirname, '../src/static'),
+    build: path.join(__dirname, '../src/static_dist'),
 };
 
 const VENDOR = [
-  'history',
-  'babel-polyfill',
-  'react',
-  'react-dom',
-  'react-redux',
-  'react-router',
-  'react-mixin',
-  'classnames',
-  'redux',
-  'react-router-redux',
-  'jquery'
+    'history',
+    'babel-polyfill',
+    'react',
+    'react-dom',
+    'react-redux',
+    'react-router',
+    'react-mixin',
+    'classnames',
+    'redux',
+    'react-router-redux',
+    'jquery'
 ];
 
 process.env.BABEL_ENV = TARGET;
 
 const common = {
-  entry: {
-    app: PATHS.app,
-    vendor: VENDOR,
-  },
+    entry: {
+        app: PATHS.app,
+        vendor: VENDOR,
+    },
 
-  output : {
-    filename   : '[name].[hash].js',
-    path       : PATHS.build,
-    publicPath : '/static/'
-  },
-  
-  plugins: [
-    new HtmlWebpackPlugin({
-      template : path.join(__dirname, '../src/static/index.html'), 
-      hash     : true,
-      filename : 'index.html',
-      inject   : 'body'
-    }),
-    new webpack.ProvidePlugin({
-      $: "jquery",
-      jQuery: "jquery",
-      "window.jQuery": "jquery"
-    }),
-    // extract all common modules to vendor so we can load multiple apps in one page
-    new webpack.optimize.CommonsChunkPlugin({ name: 'vendor', filename: 'vendor.bundle.js' })
-  ],
-  
-  resolve: {
-    extensions: ['', '.jsx', '.js', '.json', '.scss'],
-    modulesDirectories: ['node_modules', PATHS.app],
-  },
+    output: {
+        filename: '[name].[hash].js',
+        path: PATHS.build,
+        publicPath: '/static/'
+    },
 
-  module: {
-    loaders: [{
-      test: /bootstrap-sass\/assets\/javascripts\//,
-      loader: 'imports?jQuery=jquery',
-    }, {
-      test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
-      loader: 'url?limit=10000&mimetype=application/font-woff',
-    }, {
-      test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
-      loader: 'url?limit=10000&mimetype=application/font-woff2',
-    }, {
-      test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
-      loader: 'url?limit=10000&mimetype=application/octet-stream',
-    }, {
-      test: /\.otf(\?v=\d+\.\d+\.\d+)?$/,
-      loader: 'url?limit=10000&mimetype=application/font-otf',
-    }, {
-      test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
-      loader: 'file',
-    }, {
-      test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
-      loader: 'url?limit=10000&mimetype=image/svg+xml',
-    }, {
-      test: /\.js$/,
-      loaders: ['babel-loader'],
-      exclude: /node_modules/,
-    }, {
-      test: /\.png$/,
-      loader: 'file?name=[name].[ext]',
-    }, {
-      test: /\.jpg$/,
-      loader: 'file?name=[name].[ext]',
-    }],
-  },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, '../src/static/index.html'),
+            hash: true,
+            filename: 'index.html',
+            inject: 'body'
+        }),
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery',
+            'window.jQuery': 'jquery'
+        }),
+        // extract all common modules to vendor so we can load multiple apps in one page
+        new webpack.optimize.CommonsChunkPlugin({ name: 'vendor', filename: 'vendor.bundle.js' })
+    ],
 
-  postcss: (webpack) => {
-    return [
-      autoprefixer({
-        browsers: ['last 2 versions'],
-      }),
-      postcssImport({
-        addDependencyTo: webpack,
-      }),
-    ];
-  },
+    resolve: {
+        extensions: ['', '.jsx', '.js', '.json', '.scss'],
+        modulesDirectories: ['node_modules', PATHS.app],
+    },
+
+    module: {
+        loaders: [
+            {
+                test: /bootstrap-sass\/assets\/javascripts\//,
+                loader: 'imports?jQuery=jquery',
+            },
+            {
+                test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
+                loader: 'url?limit=10000&mimetype=application/font-woff',
+            }, {
+                test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
+                loader: 'url?limit=10000&mimetype=application/font-woff2',
+            }, {
+                test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+                loader: 'url?limit=10000&mimetype=application/octet-stream',
+            }, {
+                test: /\.otf(\?v=\d+\.\d+\.\d+)?$/,
+                loader: 'url?limit=10000&mimetype=application/font-otf',
+            }, {
+                test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+                loader: 'file',
+            }, {
+                test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+                loader: 'url?limit=10000&mimetype=image/svg+xml',
+            }, {
+                test: /\.js$/,
+                loaders: ['babel-loader'],
+                exclude: /node_modules/,
+            }, {
+                test: /\.png$/,
+                loader: 'file?name=[name].[ext]',
+            }, {
+                test: /\.jpg$/,
+                loader: 'file?name=[name].[ext]',
+            }],
+    },
+
+    postcss: (param) => {
+        return [
+            autoprefixer({
+                browsers: ['last 2 versions'],
+            }),
+            postcssImport({
+                addDependencyTo: param,
+            }),
+        ];
+    },
 };
 
 if (TARGET === 'dev' || !TARGET) {
-  module.exports = merge(development, common);
+    module.exports = merge(development, common);
 }
 
 if (TARGET === 'compile' || !TARGET) {
-  module.exports = merge(production, common);
+    module.exports = merge(production, common);
 }
--- webpack/dev.config.js
+++ webpack/dev.config.js
@@ -2,34 +2,34 @@ const webpack = require('webpack');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
-  devtool: 'source-map',
-  
-  entry: {
-    vendor: [
-      'bootstrap-loader',
-      'font-awesome-webpack!./src/static/styles/font-awesome.config.js'
-    ]
-  },
-  
-  output: {
-    publicPath: '../src/static_dist/',
-  },
+    devtool: 'source-map', // 'cheap-module-eval-source-map',
 
-  module: {
-    loaders: [{
-      test: /\.scss$/,
-      loader: 'style!css?localIdentName=[path][name]--[local]!postcss-loader!sass',
-    }],
-  },
+    entry: {
+        vendor: [
+            'bootstrap-loader',
+            'font-awesome-webpack!./src/static/styles/font-awesome.config.js'
+        ]
+    },
 
-  plugins: [
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: '"development"',
-      },
-      __DEVELOPMENT__: true,
-    }),
-    new ExtractTextPlugin('[name].[contenthash].css'),
-    new webpack.optimize.OccurrenceOrderPlugin()
-  ],
+    output: {
+        publicPath: '../src/static_dist/',
+    },
+
+    module: {
+        loaders: [{
+            test: /\.scss$/,
+            loader: 'style!css?localIdentName=[path][name]--[local]!postcss-loader!sass',
+        }],
+    },
+
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: '"development"',
+            },
+            __DEVELOPMENT__: true,
+        }),
+        new ExtractTextPlugin('[name].[contenthash].css'),
+        new webpack.optimize.OccurrenceOrderPlugin()
+    ],
 };
