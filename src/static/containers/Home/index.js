8bc6d28b40ec3b63f3fb1b3a99db8dd7c00f2bb5
Ruben Almeida <almeidarruben@gmail.com>
2016-06-03 15:54:22 +0100

    Add foundation grid. Update project styles to foundation. Update styles structure. Update tests

--- src/static/containers/Home/index.js
+++ src/static/containers/Home/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router';
 import { connect } from 'react-redux';
-
 import logoImage from './images/react-logo.png';
 
 class HomeView extends React.Component {
@@ -12,11 +11,33 @@ class HomeView extends React.Component {
 
     render() {
         return (
-            <div>
-                <h1>React Redux Auth Example</h1>
-                <img src={logoImage} className="pull-right" alt="React Logo"/>
-                {this.props.statusText ? <div className="alert alert-info">{this.props.statusText}</div> : ''}
-                <p>Attempt to access some <Link to="/protected">protected content.</Link></p>
+            <div className="container">
+                <div className="row margin-top-medium">
+                    <div className="small-6 columns">
+                        <h1>React Redux Auth Example</h1>
+                    </div>
+                    <div className="small-6 columns">
+                        <img src={logoImage}
+                             alt="React Logo"
+                        />
+                    </div>
+                </div>
+                <div className="row margin-top-medium">
+                    <div className="small-12 columns">
+                        <h2>Attempt to access some <Link to="/protected"><b>protected content</b>.</Link></h2>
+                    </div>
+                </div>
+                <div className="row margin-top-medium">
+                    <div className="small-12 columns">
+                        {this.props.statusText ?
+                            <div className="alert alert__info">
+                                {this.props.statusText}
+                            </div>
+                            :
+                            null
+                        }
+                    </div>
+                </div>
             </div>
         );
     }
