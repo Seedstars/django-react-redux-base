272fdc70c0504bf23cfcff7dd6a0e85917208228
Luis Rodrigues <lfrodrigues@gmail.com>
2017-08-25 13:39:01 +0200

    Removed old switch/case hack that we don't need in ES6

--- src/static/reducers/auth.js
+++ src/static/reducers/auth.js
@@ -1,4 +1,3 @@
-import { createReducer } from '../utils';
 import {
     AUTH_LOGIN_USER_REQUEST,
     AUTH_LOGIN_USER_SUCCESS,
@@ -15,37 +14,42 @@ const initialState = {
     statusText: null
 };
 
-export default createReducer(initialState, {
-    [AUTH_LOGIN_USER_REQUEST]: (state, payload) => {
-        return Object.assign({}, state, {
-            isAuthenticating: true,
-            statusText: null
-        });
-    },
-    [AUTH_LOGIN_USER_SUCCESS]: (state, payload) => {
-        return Object.assign({}, state, {
-            isAuthenticating: false,
-            isAuthenticated: true,
-            token: payload.token,
-            userName: payload.user.email,
-            statusText: 'You have been successfully logged in.'
-        });
-    },
-    [AUTH_LOGIN_USER_FAILURE]: (state, payload) => {
-        return Object.assign({}, state, {
-            isAuthenticating: false,
-            isAuthenticated: false,
-            token: null,
-            userName: null,
-            statusText: `Authentication Error: ${payload.status} - ${payload.statusText}`
-        });
-    },
-    [AUTH_LOGOUT_USER]: (state, payload) => {
-        return Object.assign({}, state, {
-            isAuthenticated: false,
-            token: null,
-            userName: null,
-            statusText: 'You have been successfully logged out.'
-        });
+export default function authReducer(state = initialState, action) {
+    switch (action.type) {
+        case AUTH_LOGIN_USER_REQUEST:
+            return Object.assign({}, state, {
+                isAuthenticating: true,
+                statusText: null
+            });
+
+        case AUTH_LOGIN_USER_SUCCESS:
+            return Object.assign({}, state, {
+                isAuthenticating: false,
+                isAuthenticated: true,
+                token: action.payload.token,
+                userName: action.payload.user.email,
+                statusText: 'You have been successfully logged in.'
+            });
+
+        case AUTH_LOGIN_USER_FAILURE:
+            return Object.assign({}, state, {
+                isAuthenticating: false,
+                isAuthenticated: false,
+                token: null,
+                userName: null,
+                statusText: `Authentication Error: ${action.payload.status} - ${action.payload.statusText}`
+            });
+
+        case AUTH_LOGOUT_USER:
+            return Object.assign({}, state, {
+                isAuthenticated: false,
+                token: null,
+                userName: null,
+                statusText: 'You have been successfully logged out.'
+            });
+
+        default:
+            return state;
     }
-});
+}
+
--- src/static/reducers/data.js
+++ src/static/reducers/data.js
@@ -1,21 +1,27 @@
-import { createReducer } from '../utils';
-import { DATA_RECEIVE_PROTECTED_DATA, DATA_FETCH_PROTECTED_DATA_REQUEST } from '../constants';
+import {
+    DATA_RECEIVE_PROTECTED_DATA,
+    DATA_FETCH_PROTECTED_DATA_REQUEST
+} from '../constants';
 
 const initialState = {
     data: null,
     isFetching: false
 };
 
-export default createReducer(initialState, {
-    [DATA_RECEIVE_PROTECTED_DATA]: (state, payload) => {
-        return Object.assign({}, state, {
-            data: payload.data,
-            isFetching: false
-        });
-    },
-    [DATA_FETCH_PROTECTED_DATA_REQUEST]: (state, payload) => {
-        return Object.assign({}, state, {
-            isFetching: true
-        });
+export default function dataReducer(state = initialState, action) {
+    switch (action.type) {
+        case DATA_RECEIVE_PROTECTED_DATA:
+            return Object.assign({}, state, {
+                data: action.payload.data,
+                isFetching: false
+            });
+
+        case DATA_FETCH_PROTECTED_DATA_REQUEST:
+            return Object.assign({}, state, {
+                isFetching: true
+            });
+        default:
+            return state;
     }
-});
+}
+
