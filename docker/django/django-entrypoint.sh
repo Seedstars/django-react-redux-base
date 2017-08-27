bdc1a691844cda1063687d6df6e38a9b8f09a0e8
Luis Rodrigues <lfrodrigues@gmail.com>
2017-08-27 14:23:04 +0200

    Updated settings for Docker

--- docker/django/django-entrypoint.sh
+++ docker/django/django-entrypoint.sh
@@ -5,11 +5,11 @@ do
     echo "Waiting for django volume..."
 done
 
-until python manage.py migrate --settings=djangoreactredux.settings.dev
+until python manage.py migrate --settings=djangoreactredux.settings.dev_docker
 do
     echo "Waiting for postgres ready..."
     sleep 2
 done
 
-python manage.py loaddata fixtures.json --settings=djangoreactredux.settings.dev
-python manage.py runserver 0.0.0.0:8000 --settings=djangoreactredux.settings.dev
+python manage.py loaddata fixtures.json --settings=djangoreactredux.settings.dev_docker
+python manage.py runserver 0.0.0.0:8000 --settings=djangoreactredux.settings.dev_docker
---
+++ src/djangoreactredux/settings/dev_docker.py
@@ -0,0 +1,14 @@
+from djangoreactredux.settings.dev import *  # NOQA (ignore all errors on this line)
+
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'djangoreactredux_dev',
+        'USER': 'djangoreactredux',
+        'PASSWORD': 'password',
+        'HOST': 'postgres',
+        'PORT': 5432,
+    }
+}
