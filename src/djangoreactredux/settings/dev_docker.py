33af48a46efc2beca172aba9bba9d6d69a7e3198
Luis Rodrigues <lfrodrigues@gmail.com>
2017-08-27 14:28:30 +0200

    Fixed static validation

--- src/djangoreactredux/settings/dev_docker.py
+++ src/djangoreactredux/settings/dev_docker.py
@@ -1,7 +1,5 @@
 from djangoreactredux.settings.dev import *  # NOQA (ignore all errors on this line)
 
-
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
